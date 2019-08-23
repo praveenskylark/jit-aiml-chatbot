42ad8b00ee3f1e661f2d5b6f02c4b2b3cbb22cfc
praveenskylark <skylarkpraveen@gmail.com>
2019-08-23 22:15:44 +0530

    published

---
+++ app/hello.py
@@ -0,0 +1,27 @@
+import aiml
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+kernel = aiml.Kernel()
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "..\startup\std-startup.xml", commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    while True:
+        message= request.args.get('msg')
+        if message == "quit":
+            exit()
+        elif message == "save":
+            kernel.saveBrain("bot_brain.brn")
+        else:
+            bot_response = kernel.respond(message)
+            return bot_response
+    
